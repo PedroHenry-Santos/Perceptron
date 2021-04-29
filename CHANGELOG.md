515e094d6cb4756beb3688effca3a3a5ddd83eab
PedroSantos <pedrohenry.viana@gmail.com>
2021-04-29 15:05:30 -0300

    feat(lib): adds data types and definitions used in the application

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+# 1.0.0 (2021-04-29)
+
+
+### Features
+
+* **components:** adds visualization components for general application use ([e30001c](https://github.com/PedroHenry-Santos/Perceptron/commit/e30001ce7d192d021bc39a40e38bbcdf5d0227e4))
--- src/index.c
+++ src/index.c
@@ -1,9 +1,21 @@
+/*
+* Autor: Pedro Henrique Viana dos Santos.
+* Purpose: Implement a perceptron application with an interactive interface.
+* Language: C.
+*/
+
+// Libraries
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include "lib/__application/startup.h"
 
-int main(int argc, char const *argv[])
-{
-    printf("Hello World!");
+// Imports components
+#include "./components/main.c"
+#include "./utils/checkState.c"
+
+int main(int argc, char const *argv[]) {
+    srand(time(NULL));
 
     return 0;
 }
